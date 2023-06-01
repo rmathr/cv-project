34fb14f1277f0fa6f98aa4973d877813fe67ff28
Matheus Rodrigues <85845749+rmathr@users.noreply.github.com>
2023-06-01 10:46:37 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+      ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader, // instead of style-loader
+                'css-loader'
+              ],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+          }
+        ],
+      },
+};
