f2ed1cd822972e4024efb5301ec98221885c829f
Matheus <85845749+rmathr@users.noreply.github.com>
2023-06-10 19:22:27 -0400

    Change favicon and adjust space between main elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './style.css';
+import cv from './assets/icons/cv.png';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const handleIcon = (function () {
+  const link =
+    document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/png';
+  link.rel = 'shortcut icon';
+  link.href = cv;
+  document.getElementsByTagName('head')[0].appendChild(link);
+})();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
